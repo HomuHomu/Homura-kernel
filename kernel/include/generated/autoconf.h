6b8bcaa48e3ebe002dcdb2d5ebb21ba827f0c128
HomuHomu <Homu@Homu.com>
2011-11-14 01:27:39 +0900

    CM7:Only turn off touchkey ldos in early_suspend.

--- kernel/include/generated/autoconf.h
+++ kernel/include/generated/autoconf.h
@@ -1,7 +1,7 @@
 /*
  * Automatically generated C config: don't edit
  * Linux kernel version: 2.6.35.7
- * Fri Nov 11 22:42:36 2011
+ * Mon Nov 14 00:45:50 2011
  */
 #define AUTOCONF_INCLUDED
 #define CONFIG_NF_CONNTRACK_H323 1
--- kernel/include/generated/compile.h
+++ kernel/include/generated/compile.h
@@ -1,8 +1,8 @@
-/* This file is auto generated, version CyanogenMod1 */
+/* This file is auto generated, version CyanogenMod2 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#CyanogenMod1 SMP PREEMPT Fri Nov 11 21:33:35 JST 2011"
-#define LINUX_COMPILE_TIME "21:33:35"
+#define UTS_VERSION "#CyanogenMod2 SMP PREEMPT Mon Nov 14 00:45:52 JST 2011"
+#define LINUX_COMPILE_TIME "00:45:52"
 #define LINUX_COMPILE_BY "Homura"
 #define LINUX_COMPILE_HOST "Akemi"
 #define LINUX_COMPILE_DOMAIN "(none)"
