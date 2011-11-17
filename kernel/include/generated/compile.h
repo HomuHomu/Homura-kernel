1ecbbc0963bc364f08594015ffed00e47d533c58
HomuHomu <ecoco666@gmail.com>
2011-11-18 00:41:45 +0900

    XData DualBoot Working

--- kernel/include/generated/compile.h
+++ kernel/include/generated/compile.h
@@ -1,8 +1,8 @@
-/* This file is auto generated, version DCM9 */
+/* This file is auto generated, version Dualboot4 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#DCM9 SMP PREEMPT Thu Nov 17 19:55:18 JST 2011"
-#define LINUX_COMPILE_TIME "19:55:18"
+#define UTS_VERSION "#Dualboot4 SMP PREEMPT Fri Nov 18 00:35:41 JST 2011"
+#define LINUX_COMPILE_TIME "00:35:41"
 #define LINUX_COMPILE_BY "Homura"
 #define LINUX_COMPILE_HOST "Akemi"
 #define LINUX_COMPILE_DOMAIN "(none)"
