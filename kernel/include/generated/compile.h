8e10ddc519983db765080004f105bec9c5601c9b
HomuHomu <ecoco666@gmail.com>
2011-11-24 01:42:08 +0900

    Homura-kernel-Dual9

--- kernel/include/generated/compile.h
+++ kernel/include/generated/compile.h
@@ -1,8 +1,8 @@
-/* This file is auto generated, version Dualboot8 */
+/* This file is auto generated, version Dual9 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#Dualboot8 SMP PREEMPT Tue Nov 22 02:48:18 JST 2011"
-#define LINUX_COMPILE_TIME "02:48:18"
+#define UTS_VERSION "#Dual9 SMP PREEMPT Thu Nov 24 01:37:05 JST 2011"
+#define LINUX_COMPILE_TIME "01:37:05"
 #define LINUX_COMPILE_BY "Homura"
 #define LINUX_COMPILE_HOST "Akemi"
 #define LINUX_COMPILE_DOMAIN "(none)"
