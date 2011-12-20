71eb1de005bc8f8c0e264eaec58b3846dc0bc771
HomuHomu <ecoco666@gmail.com>
2011-12-21 00:28:47 +0900

    Change Homura_boot

--- kernel/include/generated/autoconf.h
+++ kernel/include/generated/autoconf.h
@@ -1,7 +1,7 @@
 /*
  * Automatically generated C config: don't edit
  * Linux kernel version: 2.6.35.7
- * Tue Dec 20 23:10:17 2011
+ * Wed Dec 21 00:23:44 2011
  */
 #define AUTOCONF_INCLUDED
 #define CONFIG_NF_CONNTRACK_H323 1
