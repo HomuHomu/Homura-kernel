19d9479fbcb38885fe44188645e0daacddb7042c
HomuHomu <ecoco666@gmail.com>
2012-01-03 14:38:52 +0900

    Homura-kernel rev17

--- build_kernel_aosp.sh
+++ build_kernel_aosp.sh
@@ -12,7 +12,7 @@ KERNEL_NAME="Homura-Kernel"
 CONFIG_FILE="$KERNEL_PATH/arch/arm/configs/Homura_aosp_defconfig"
 
 export LOCALVERSION="-$KERNEL_NAME"
-export KBUILD_BUILD_VERSION="aosp16"
+export KBUILD_BUILD_VERSION="aosp17"
 export WHOAMI_MOD="Homura"
 export HOSTNAME_MOD="Akemi"
 
--- kernel/include/generated/autoconf.h
+++ kernel/include/generated/autoconf.h
@@ -1,7 +1,7 @@
 /*
  * Automatically generated C config: don't edit
  * Linux kernel version: 2.6.35.7
- * Tue Jan  3 12:28:14 2012
+ * Tue Jan  3 14:22:30 2012
  */
 #define AUTOCONF_INCLUDED
 #define CONFIG_NF_CONNTRACK_H323 1
