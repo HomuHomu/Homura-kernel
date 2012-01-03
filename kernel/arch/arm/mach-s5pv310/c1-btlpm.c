5de43e9ad16bebff350a00398fe1991b9a908f9e
HomuHomu <ecoco666@gmail.com>
2012-01-03 13:55:09 +0900

    c1-btlpm update again

--- kernel/arch/arm/mach-s5pv310/c1-btlpm.c
+++ kernel/arch/arm/mach-s5pv310/c1-btlpm.c
@@ -14,6 +14,9 @@
 #include <linux/err.h>
 #include <linux/gpio.h>
 #include <linux/hrtimer.h>
+#include <linux/platform_device.h>
+#include <linux/device.h>
+#include <linux/miscdevice.h>
 
 #include <asm/mach-types.h>
 
@@ -21,50 +24,102 @@
 
 #include "c1.h"
 
+#include <linux/wakelock.h>
+static struct wake_lock bt_playback_wake_lock;
+
 static struct c1_bt_lpm {
-	struct hrtimer bt_lpm_timer;
-	ktime_t bt_lpm_delay;
+    struct hrtimer bt_lpm_timer;
+    ktime_t bt_lpm_delay;
 } bt_lpm;
 
 static enum hrtimer_restart bt_enter_lpm(struct hrtimer *timer)
 {
-	gpio_set_value(GPIO_BT_WAKE, 0);
+    wake_unlock(&bt_playback_wake_lock);
+    gpio_set_value(GPIO_BT_WAKE, 0);
     pr_debug("[BT] GPIO_BT_WAKE = %d\n", gpio_get_value(GPIO_BT_WAKE) );
 
-	return HRTIMER_NORESTART;
+    return HRTIMER_NORESTART;
 }
 
 void c1_bt_uart_wake_peer(struct uart_port *port)
 {
-	if (!bt_lpm.bt_lpm_timer.function)
-		return;
+    if (!bt_lpm.bt_lpm_timer.function)
+        return;
 
-	hrtimer_try_to_cancel(&bt_lpm.bt_lpm_timer);
-	if (!gpio_get_value(GPIO_BT_WAKE)) {
-		gpio_set_value(GPIO_BT_WAKE, 1);
-		pr_debug("[BT] GPIO_BT_WAKE = %d\n", gpio_get_value(GPIO_BT_WAKE) );
-	}
-	hrtimer_start(&bt_lpm.bt_lpm_timer, bt_lpm.bt_lpm_delay, HRTIMER_MODE_REL);
+    hrtimer_try_to_cancel(&bt_lpm.bt_lpm_timer);
+    if (!gpio_get_value(GPIO_BT_WAKE)) {
+        gpio_set_value(GPIO_BT_WAKE, 1);
+        wake_lock(&bt_playback_wake_lock);
+        pr_debug("[BT] GPIO_BT_WAKE = %d\n", gpio_get_value(GPIO_BT_WAKE) );
+    }
+    hrtimer_start(&bt_lpm.bt_lpm_timer, bt_lpm.bt_lpm_delay, HRTIMER_MODE_REL);
 }
+
+#ifdef CONFIG_BUILD_TARGET_DUALBOOT
+static ssize_t bt_mode_store(struct device *dev, struct device_attribute *attr, const char *buf, size_t size)
+{
+    int ret, value = 0;
+    sscanf(buf, "%d", &value);
+
+    if (value != 0) {
+        if (!machine_is_c1())
+            return 0;
+
+        ret = gpio_request(GPIO_BT_WAKE, "gpio_bt_wake");
+        if (ret) {
+            pr_err("Failed to request gpio_bt_wake control\n");
+            return 0;
+        }
+
+        wake_lock_init(&bt_playback_wake_lock, WAKE_LOCK_SUSPEND, "bt_playback");
+
+        gpio_direction_output(GPIO_BT_WAKE, GPIO_LEVEL_LOW);
+
+        hrtimer_init(&bt_lpm.bt_lpm_timer, CLOCK_MONOTONIC, HRTIMER_MODE_REL);
+        bt_lpm.bt_lpm_delay = ktime_set(1, 0);	/* 1 sec */
+        bt_lpm.bt_lpm_timer.function = bt_enter_lpm;
+    }
+    return size;
+}
+
+static DEVICE_ATTR(bt_mode, 0664, NULL, bt_mode_store);
+
+static struct miscdevice bt_lpm_device = {
+    .minor = MISC_DYNAMIC_MINOR,
+    .name = "bt_lpm",
+};
+#endif
 
 static int __init bt_lpm_init(void)
 {
-	int ret;
+    int ret;
 
-	if (!machine_is_c1())
-		return 0;
+#ifdef CONFIG_BUILD_TARGET_DUALBOOT
+    ret = misc_register(&bt_lpm_device);
+    if (ret) {
+        pr_err("%s misc_register(%s) fail\n", __func__, bt_lpm_device.name);
+        return 1;
+    }
 
-	ret = gpio_request(GPIO_BT_WAKE, "gpio_bt_wake");
-	if (ret) {
-		pr_err("Failed to request gpio_bt_wake control\n");
-		return 0;
-	}
+    if (sysfs_create_file(&bt_lpm_device.this_device->kobj, &dev_attr_bt_mode.attr) < 0)
+        pr_err("Failed to create device file(%s)!\n", dev_attr_bt_mode.attr.name);
+#else
+    if (!machine_is_c1())
+        return 0;
 
-	gpio_direction_output(GPIO_BT_WAKE, GPIO_LEVEL_LOW);
+    ret = gpio_request(GPIO_BT_WAKE, "gpio_bt_wake");
+    if (ret) {
+        pr_err("Failed to request gpio_bt_wake control\n");
+        return 0;
+    }
+ 
+    wake_lock_init(&bt_playback_wake_lock, WAKE_LOCK_SUSPEND, "bt_playback");
 
-	hrtimer_init(&bt_lpm.bt_lpm_timer, CLOCK_MONOTONIC, HRTIMER_MODE_REL);
-	bt_lpm.bt_lpm_delay = ktime_set(1, 0);	/* 1 sec */
-	bt_lpm.bt_lpm_timer.function = bt_enter_lpm;
-	return 0;
+    gpio_direction_output(GPIO_BT_WAKE, GPIO_LEVEL_LOW);
+    hrtimer_init(&bt_lpm.bt_lpm_timer, CLOCK_MONOTONIC, HRTIMER_MODE_REL);
+    bt_lpm.bt_lpm_delay = ktime_set(1, 0); /* 1 sec */
+    bt_lpm.bt_lpm_timer.function = bt_enter_lpm;
+#endif
+    return 0;
 }
 device_initcall(bt_lpm_init);
