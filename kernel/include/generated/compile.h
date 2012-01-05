f588f9655f217c146c8e28f4a587d067fc5498be
HomuHomu <ecoco666@gmail.com>
2012-01-05 21:07:51 +0900

    Supports Voodoo  LOUDER

--- kernel/include/generated/compile.h
+++ kernel/include/generated/compile.h
@@ -1,8 +1,8 @@
-/* This file is auto generated, version Dual16 */
+/* This file is auto generated, version DCM17 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#Dual16 SMP PREEMPT Tue Dec 27 00:07:51 JST 2011"
-#define LINUX_COMPILE_TIME "00:07:51"
+#define UTS_VERSION "#DCM17 SMP PREEMPT Thu Jan 5 20:53:10 JST 2012"
+#define LINUX_COMPILE_TIME "20:53:10"
 #define LINUX_COMPILE_BY "Homura"
 #define LINUX_COMPILE_HOST "Akemi"
 #define LINUX_COMPILE_DOMAIN "(none)"
