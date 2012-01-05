45b55f08c36a13ae9e0ea6130cf697a55156f32a
HomuHomu <ecoco666@gmail.com>
2012-01-05 22:30:08 +0900

    Homura-kernel rev18

--- kernel/include/generated/autoconf.h
+++ kernel/include/generated/autoconf.h
@@ -1,7 +1,7 @@
 /*
  * Automatically generated C config: don't edit
  * Linux kernel version: 2.6.35.7
- * Thu Jan  5 21:12:27 2012
+ * Thu Jan  5 22:24:35 2012
  */
 #define AUTOCONF_INCLUDED
 #define CONFIG_NF_CONNTRACK_H323 1
--- kernel/include/generated/compile.h
+++ kernel/include/generated/compile.h
@@ -1,8 +1,8 @@
-/* This file is auto generated, version Dual17 */
+/* This file is auto generated, version Dual18 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#Dual17 SMP PREEMPT Thu Jan 5 21:12:29 JST 2012"
-#define LINUX_COMPILE_TIME "21:12:29"
+#define UTS_VERSION "#Dual18 SMP PREEMPT Thu Jan 5 22:24:38 JST 2012"
+#define LINUX_COMPILE_TIME "22:24:38"
 #define LINUX_COMPILE_BY "Homura"
 #define LINUX_COMPILE_HOST "Akemi"
 #define LINUX_COMPILE_DOMAIN "(none)"
