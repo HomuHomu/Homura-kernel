dc8397b39f025278a74e46c5116b77c9af6aedff
HomuHomu <ecoco666@gmail.com>
2012-01-20 22:34:18 +0900

    Change Lulzactive Gov

--- kernel/include/generated/autoconf.h
+++ kernel/include/generated/autoconf.h
@@ -1,7 +1,7 @@
 /*
  * Automatically generated C config: don't edit
  * Linux kernel version: 2.6.35.7
- * Fri Jan 20 19:37:02 2012
+ * Fri Jan 20 22:26:05 2012
  */
 #define AUTOCONF_INCLUDED
 #define CONFIG_NF_CONNTRACK_H323 1
