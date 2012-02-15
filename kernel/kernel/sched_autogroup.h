56f81d4d7c2b5b76d55cee263d4d7b4381304e67
Yong Zhang <yong.zhang0@gmail.com>
2012-02-15 23:26:48 +0900

    sched, autogroup: stop going ahead if autogroup is disable

--- kernel/kernel/sched_autogroup.h
+++ kernel/kernel/sched_autogroup.h
@@ -1,11 +1,25 @@
 #ifdef CONFIG_SCHED_AUTOGROUP
 
+struct autogroup {
+	/*
+	 * reference doesn't mean how many thread attach to this
+	 * autogroup now. It just stands for the number of task
+	 * could use this autogroup.
+	 */
+	struct kref		kref;
+	struct task_group	*tg;
+	struct rw_semaphore	lock;
+	unsigned long		id;
+	int			nice;
+};
+
 static inline struct task_group *
 autogroup_task_group(struct task_struct *p, struct task_group *tg);
 
 #else /* !CONFIG_SCHED_AUTOGROUP */
 
 static inline void autogroup_init(struct task_struct *init_task) {  }
+static inline void autogroup_free(struct task_group *tg) { }
 
 static inline struct task_group *
 autogroup_task_group(struct task_struct *p, struct task_group *tg)
@@ -21,3 +35,4 @@ static inline int autogroup_path(struct task_group *tg, char *buf, int buflen)
 #endif
 
 #endif /* CONFIG_SCHED_AUTOGROUP */
+
